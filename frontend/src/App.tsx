5435fcf65885c354d410a5a459a9a08d8c5916b8
Saiprabhu007 <saiprabhu.dandanayak@zemosolabs.com>
2024-04-15 14:39:22 +0530

    ✨ feat : integrated eslint to react application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => <h1>your app name</h1>
+const App = () => <h1>your app name</h1>;
 
-export default App
+export default App;
+
+
